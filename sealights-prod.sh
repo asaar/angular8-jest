960dd4598a7e579fc3a7a54dcf8fa54303cbc0cb
asaar <34831404+asaar@users.noreply.github.com>
2020-01-02 12:34:54 +0200

    Create sealights-prod.sh

---
+++ sealights-prod.sh
@@ -0,0 +1,15 @@
+#Increment build number
+export a=`cat num`
+export a=`expr $a + 1`
+echo $a > num
+
+#Install SL agent
+npm i slnodejs
+export NODE_DEBUG=sl
+./node_modules/.bin/slnodejs config --tokenfile sltoken.txt --appname Angular8-Jest_Repro --branch master --build `cat num`
+npm run-script build-prod
+./node_modules/.bin/slnodejs build --tokenfile sltoken.txt --buildsessionidfile buildSessionId --workspacepath dist --scm git --projectroot `pwd`
+npm test
+./node_modules/.bin/slnodejs start --tokenfile sltoken.txt --buildsessionidfile buildSessionId --teststage "Jest Tests"
+./node_modules/.bin/slnodejs nycReport --tokenfile sltoken.txt --buildsessionidfile buildSessionId 
+./node_modules/.bin/slnodejs end --tokenfile sltoken.txt --buildsessionidfile buildSessionId
